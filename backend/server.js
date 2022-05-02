0a73c2486740da5b489fed15f66aba7d82cc68f9
Thummar Darshankumar <73601117+dashu1999@users.noreply.github.com>
2022-05-02 22:32:40 +0500

    pub heroku

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/AmazonShop');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/AmazonShop');
 
 // const URI = "mongodb://127.0.0.1:27017/AmazonShop"
 
