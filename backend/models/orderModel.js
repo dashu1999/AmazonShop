cd3060d1bf00c57c7af11d096c0bd710d7dc671e
Thummar Darshankumar <73601117+dashu1999@users.noreply.github.com>
2022-05-15 00:20:47 +0500

    53 Order Recipt Send By Email

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -21,6 +21,7 @@ const orderSchema = new mongoose.Schema(
             city: { type: String, required: true },
             postalCode: { type: String, required: true },
             country: { type: String, required: true },
+            phone: { type: Number, required: true },
             lat: Number,
             lng: Number,
         },
--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAdmin, isAuth, isSellerOrAdmin } from "../utils.js";
+import { isAdmin, isAuth, isSellerOrAdmin, mailgun, payOrderEmailTemplate } from "../utils.js";
 
 
 
@@ -68,23 +68,52 @@ orderRouter.get('/:id', isAuth, expressAsyncHandler(async (req, res) => {
   }
 }));
 
-orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
-  if (order) {
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.paymentResult = {
-      id: req.body.id,
-      status: req.body.status,
-      update_time: req.body.update_time,
-      email_address: req.body.email_address,
-    };
-    const updatedOrder = await order.save();
-    res.send({ message: "Order Paid", order: updatedOrder });
-  } else {
-    res.status(404).send({ message: "Order Not Found" });
-  }
-}));
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'email name'
+    );
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      try {
+        mailgun()
+          .messages()
+          .send(
+            {
+              from: 'Amazona <amazona@mg.yourdomain.com>',
+              to: `${order.user.name} <${order.user.email}>`,
+              subject: `New order ${order._id}`,
+              html: payOrderEmailTemplate(order),
+            },
+            (error, body) => {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log(body);
+              }
+            }
+          );
+      } catch (err) {
+        console.log(err);
+      }
+
+      res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
 
 orderRouter.delete('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
